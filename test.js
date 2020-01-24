3d30d4352862924b55136582ca2fc95eb2852ace
anzerr <thoron174+anzerr@gmail.com>
2020-01-24 13:07:41 +0100

    🚧 dump

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ const uint = require('./index.js'),
 let value = [10, 100], b = uint.uint8.toHex(value);
 
 assert.equal(b, Buffer.from(value).toString('hex').toUpperCase());
-assert.deepEqual(uint.uint8.fromHex(b), value);
+assert.deepEqual(uint.uint8.fromHex(b), new Uint8Array(value));
 assert.deepEqual(uint.uint8.toUint4(value), [0, 10, 6, 4]);
